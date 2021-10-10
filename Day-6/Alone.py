1d464c1fd7bfb700a68ca66c43a558315ebba325
Nitish Thakur <47694054+nk300498@users.noreply.github.com>
2021-10-10 20:18:50 +0530

    Add files via upload

---
+++ Day-6/Alone.py
@@ -0,0 +1,13 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+    
+turn_right()
+
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
